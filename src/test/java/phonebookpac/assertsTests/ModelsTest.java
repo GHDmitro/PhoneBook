24be70a853449d4af1bd4539de4d4998aea4b938
Isgaroth <romanenko.serhiy.platform@gmail.com>
2017-11-16 22:03:46 +0200

    Some changes

--- src/test/java/phonebookpac/assertsTests/ModelsTest.java
+++ src/test/java/phonebookpac/assertsTests/ModelsTest.java
@@ -5,12 +5,16 @@ import phonebookpac.PhoneBookApplicationTests;
 import phonebookpac.model.Account;
 import phonebookpac.model.Record;
 
+import java.util.LinkedHashSet;
 import java.util.UUID;
 
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 /**
- * Created by Dmytro Tymoshenko on 16.11.17.
+ * Created by Dmytro Tymoshenko and Serhiy Romanenko on 16.11.17.
  */
 public class ModelsTest extends PhoneBookApplicationTests {
 
@@ -34,6 +38,21 @@ public class ModelsTest extends PhoneBookApplicationTests {
     }
 
 
+    @Test
+    public void testMockitoModel() {
+
+        Account mockAccount = mock(Account.class);
+
+        assertTrue(mockAccount.getPhoneBook().isEmpty());
+
+        when(mockAccount.getPhoneBook()).thenReturn(new LinkedHashSet<Record>(asList(getRecord(), getRecord())));
+
+        assertEquals(mockAccount.getPhoneBook().size(), 2);
+
+        verify(mockAccount, times(2)).getPhoneBook();
+    }
+
+
     private Account getAccount(String name, String surName, String lastName) {
         return new Account("login", "password" , "password", name, surName, lastName);
 
@@ -43,6 +62,25 @@ public class ModelsTest extends PhoneBookApplicationTests {
         return new Record(name, surname, lastName, UUID.randomUUID().toString());
     }
 
+    private Record getRecord() {
 
+        return getRecord("name", "serName", "lastName");
+    }
 
+    private Account getAccount(){
+        return getAccount("name", "surName", "lastName");
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
